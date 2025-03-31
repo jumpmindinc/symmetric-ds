a538843293bde39d6bb73889db43fa177220afc5
Eric Long <erilong@users.noreply.github.com>
2025-03-31 16:30:07 -0400

    0006774: Sybase ASE unitext error when exceeding 8192 characters (#241)

--- symmetric-db/src/main/java/org/jumpmind/db/platform/ase/AseDmlStatement.java
+++ symmetric-db/src/main/java/org/jumpmind/db/platform/ase/AseDmlStatement.java
@@ -13,9 +13,12 @@ public class AseDmlStatement extends DmlStatement {
 
     @Override
     protected int getTypeCode(Column column, boolean isDateOverrideToTimestamp) {
-        if (column.getJdbcTypeName() != null && (column.getJdbcTypeName().equalsIgnoreCase("unitext")
-                || column.getJdbcTypeName().equalsIgnoreCase("unichar") || column.getJdbcTypeName().equalsIgnoreCase("univarchar"))) {
-            return Types.VARCHAR;
+        if (column.getJdbcTypeName() != null) {
+            if (column.getJdbcTypeName().equalsIgnoreCase("unichar") || column.getJdbcTypeName().equalsIgnoreCase("univarchar")) {
+                return Types.VARCHAR;
+            } else if (column.getJdbcTypeName().equalsIgnoreCase("unitext")) {
+                return Types.BLOB;
+            }
         }
         return super.getTypeCode(column, isDateOverrideToTimestamp);
     }
