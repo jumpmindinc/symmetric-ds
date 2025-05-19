7e9cedd692d55ae01177487635e0574f8a2b122f
Pavel Belov <pbelov@jumpmind.com>
2025-05-19 13:21:44 -0400

    Added isFunctionInstalled and refactored PostgreSqlSymmetricDialect.

--- symmetric-client/src/main/java/org/jumpmind/symmetric/db/postgresql/PostgreSqlSymmetricDialect.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/db/postgresql/PostgreSqlSymmetricDialect.java
@@ -24,6 +24,7 @@ import java.sql.Types;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jumpmind.db.model.Table;
 import org.jumpmind.db.platform.IDatabasePlatform;
 import org.jumpmind.db.sql.ISqlTransaction;
@@ -36,6 +37,7 @@ import org.jumpmind.symmetric.db.ISymmetricDialect;
 import org.jumpmind.symmetric.db.SequenceIdentifier;
 import org.jumpmind.symmetric.model.Trigger;
 import org.jumpmind.symmetric.service.IParameterService;
+import org.jumpmind.util.FormatUtils;
 
 /*
  * Support for PostgreSQL
@@ -45,64 +47,14 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
     static final String SYNC_TRIGGERS_DISABLED_VARIABLE = "symmetric.triggers_disabled";
     static final String SYNC_NODE_DISABLED_VARIABLE = "symmetric.node_disabled";
     static final String SQL_DROP_FUNCTION = "drop function $(functionName)";
-    static final String SQL_FUNCTION_INSTALLED = " select count(*) from information_schema.routines " +
+    public static final String SQL_FUNCTION_INSTALLED = " select count(*) from information_schema.routines " +
             " where routine_name = '$(functionName)' and specific_schema = '$(defaultSchema)'";
     static final String SQL_SELECT_TRANSACTIONS = "select min(a.xact_start) from pg_stat_activity a join pg_catalog.pg_locks l on l.pid = a.pid  where l.mode = 'RowExclusiveLock'";
     private Boolean supportsTransactionId = null;
-    static final String SQL_FUNCTION_INSTALLED_FOR_SCHEMA = " select count(*) from information_schema.routines " +
-            " where routine_name = '$(functionName)' and specific_schema = ";
     protected static String sharedTriggersDisabledFunction;
     protected static String sharedNodeDisabledFunction;
     protected static String sharedReadLargeObjectFunction;
-    protected static String sharedTruncateEventFunction;
-    static final String sharedTruncateEventFunctionTemplate = "CREATE OR REPLACE FUNCTION $(functionName)() RETURNS TRIGGER AS $$ "
-            + "DECLARE "
-            + "    command_text VARCHAR(3000); "
-            + "    argumentNo int:=0; "
-            + "    tableSchema varchar(255); "
-            + "    tableName varchar(255); "
-            + "    channelId varchar(128); "
-            + "    triggerId varchar(128); "
-            + "    histId int:=0; "
-            + "BEGIN  "
-            // -- TODO: if replication is disabled then RETURN NEW; END if;
-            + "    tableSchema := TG_TABLE_SCHEMA; "
-            + "    tableName := TG_TABLE_NAME;  "
-            + "    if (tableSchema IS NOT NULL and LENGTH(tableSchema)>0) then "
-            + "        command_text := 'delimiter $; TRUNCATE TABLE ' || tableSchema || '.'|| tableName; "
-            + "    else "
-            + "        command_text := 'delimiter $; TRUNCATE TABLE ' || tableName; "
-            + "    end if; "
-            + "    if(TG_NARGS > 1) THEN "
-            + "        for argumentNo in 1..TG_NARGS-1 LOOP  "
-            + "            command_text := command_text || ' ' ||  TG_ARGV[argumentNo ]; "
-            + "            argumentNo := argumentNo+1; "
-            + "        END LOOP; "
-            + "    end if; "
-            + "    if (tableName like '%.%') then "
-            + "           tableSchema := split_part(tableName, '.', 1); "
-            + "           tableName := split_part(tableName, '.', 2); "
-            + "    end if; "
-            + "    tableSchema := trim(both '\\\"' from tableSchema); "
-            + "    tableName := trim(both '\\\"' from tableName); "
-            + "    select tr.trigger_id, tr.channel_id, max(th.trigger_hist_id) "
-            + "      into triggerId, channelId, histId "
-            + "      from $(defaultSchema).$(prefixName)_trigger tr  "
-            + "      join $(defaultSchema).$(prefixName)_trigger_hist th "
-            + "        on th.trigger_id = tr.trigger_id and th.inactive_time is null "
-            + "      where tr.source_schema_name = lower(tableSchema) and tr.source_table_name = lower(tableName) "
-            + "      group by tr.trigger_id, tr.channel_id "
-            + "      limit 1; "
-            + "    if (channelId is null) then  "
-            + "        RAISE EXCEPTION 'Unable to capture TRUNCATE event because SymmetricDS configuration is incomplete (channel is NULL)! Table=%', tableName "
-            + "            USING MESSAGE = 'Run SyncTriggers job in SymmetricDS (Disabling this trigger will cause data to be out-of-sync!)'; "
-            + "        RETURN NEW;  "
-            + "    end if; "
-            + "    INSERT INTO $(defaultSchema).$(prefixName)_data(table_name, event_type, trigger_hist_id, row_data, channel_id, source_node_id, transaction_id, create_time) "
-            + "      VALUES (tableName, 'S', histId, command_text, channelId, 'source', TO_CHAR(CURRENT_TIMESTAMP, 'truncate-YYYY-MM-DD')||'T'||TO_CHAR(CURRENT_TIMESTAMP, 'HH24:MI:MSOF'),  CURRENT_TIMESTAMP); "
-            + "    RAISE NOTICE 'Detected truncate of the % table; command_text=%; %, TG_NARGS=%; channel=%', tableName, command_text, CURRENT_TIMESTAMP, TG_NARGS, channelId; "
-            + "    RETURN NEW;  "
-            + "END $$ LANGUAGE plpgsql;";
+
 
     public PostgreSqlSymmetricDialect(IParameterService parameterService, IDatabasePlatform platform) {
         super(parameterService, platform);
@@ -121,7 +73,6 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
         sharedTriggersDisabledFunction = this.parameterService.getTablePrefix() + "_triggers_disabled";
         sharedNodeDisabledFunction = this.parameterService.getTablePrefix() + "_node_disabled";
         sharedReadLargeObjectFunction = this.parameterService.getTablePrefix() + "_largeobject";
-        sharedTruncateEventFunction = "f" + this.parameterService.getTablePrefix() + "_on_truncate_table_confgured_for_replication";
     }
 
     @Override
@@ -139,7 +90,7 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
                 transaction.close();
             }
         }
-        if (!installed(SQL_FUNCTION_INSTALLED, sharedTriggersDisabledFunction)) {
+        if (!isFunctionInstalled(sharedTriggersDisabledFunction)) {
             String sql = "CREATE or REPLACE FUNCTION $(functionName)() RETURNS INTEGER AS $$ "
                     + "   DECLARE "
                     + "     triggerDisabled INTEGER; "
@@ -153,7 +104,7 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
             install(sql, sharedTriggersDisabledFunction, ddl);
             log.debug("Created shared function {} for tracking when triggers are disabled.", sharedTriggersDisabledFunction);
         }
-        if (!installed(SQL_FUNCTION_INSTALLED, sharedNodeDisabledFunction)) {
+        if (!isFunctionInstalled(sharedNodeDisabledFunction)) {
             String sql = "CREATE or REPLACE FUNCTION $(functionName)() RETURNS VARCHAR AS $$ "
                     + "   DECLARE "
                     + "     nodeId VARCHAR(50); "
@@ -168,7 +119,7 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
             install(sql, sharedNodeDisabledFunction, ddl);
             log.debug("Created shared function {} for tracking when node replication is disabled.", sharedNodeDisabledFunction);
         }
-        if (!installed(SQL_FUNCTION_INSTALLED, sharedReadLargeObjectFunction)) {
+        if (!isFunctionInstalled(sharedReadLargeObjectFunction)) {
             String sql = "CREATE OR REPLACE FUNCTION $(functionName)(objectId oid) RETURNS text AS $$  "
                     + "   DECLARE      "
                     + "     encodedBlob text;    "
@@ -187,94 +138,53 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
             install(sql, sharedReadLargeObjectFunction, ddl);
             log.info("Created shared function {} for processing LOBs", sharedReadLargeObjectFunction);
         }
-        createSharedTruncateCaptureFunctions(ddl);
-    }
-
-    public String[] getAllSchemas() {
-        String[] schemas = {};
-        String schemaListDelimiter = ";";
-        String getAllSchemasAvailable = "select STRING_AGG(source_schema_name, '" + schemaListDelimiter + "') as schemas from"
-                + "( select distinct tr.source_schema_name"
-                + "  from " + this.parameterService.getTablePrefix() + "_trigger as tr"
-                + "  join information_schema.schemata as sc on sc.schema_name = tr.source_schema_name)";
-        String schemaList = platform.getSqlTemplate().queryForString(getAllSchemasAvailable);
-        if (schemaList == null || schemaList.length() < 1) {
-            return schemas;
+        if (parameterService.is(ParameterConstants.TRIGGER_CAPTURE_DDL_CHANGES)) {
+            createSharedTruncateCaptureFunctions(ddl);
         }
-        schemas = schemaList.split(schemaListDelimiter);
-        return schemas;
     }
 
     public void createSharedTruncateCaptureFunctions(StringBuilder ddl) {
-        if (!parameterService.is(ParameterConstants.TRIGGER_CAPTURE_DDL_CHANGES)) {
-            log.debug("Skipped creating shared function {} because {} parameter is off.", sharedTruncateEventFunction,
-                    ParameterConstants.TRIGGER_CAPTURE_DDL_CHANGES);
+        PostgreSqlTriggerTemplate templatesMap = (PostgreSqlTriggerTemplate) this.triggerTemplate;
+        String sharedTruncateEventFunction = templatesMap.getTruncateSharedFunctionName();
+        if (isFunctionInstalled(sharedTruncateEventFunction)) {
+            log.info("Detected shared function {} for capturing table truncate events.", sharedTruncateEventFunction);
             return;
         }
-
-        try {
-            String[] schemas = getAllSchemas();
-            if (schemas == null || schemas.length < 1) {
-                log.warn(
-                        "Skipped creating shared function for monitoring truncate events because there are no schemas with tables configured for replication.");
-                return;
-            }
-            for (String schema : schemas) {
-                if (schema == null || schema.length() < 1) {
-                    continue;
-                }
-                if (!installed(SQL_FUNCTION_INSTALLED_FOR_SCHEMA + "'" + schema + "'", sharedTruncateEventFunction)) {
-                    String currentSchemaSharedTruncateEventFunction = "\"" + schema + "\".\"" + sharedTruncateEventFunction + "\"";
-                    install(sharedTruncateEventFunctionTemplate, currentSchemaSharedTruncateEventFunction, ddl);
-                    log.info(
-                            "Created shared function {} for monitoring truncate events in schema {}", currentSchemaSharedTruncateEventFunction,
-                            schema);
-                }
-            }
-        } catch (Exception ex) {
-            log.error("Failed to create shared function for monitoring truncate events in schemas with tables configured for replication.", ex);
+        String sql = templatesMap.getEntry(sharedTruncateEventFunction);
+        String defaultSchema = platform.getDefaultSchema();
+        if (!StringUtils.isBlank(defaultSchema)) {
+            defaultSchema += ".";
         }
+        sql = FormatUtils.replace("prefixName", this.parameterService.getTablePrefix(), sql);
+        sql = FormatUtils.replace("defaultSchema", defaultSchema, sql);
+        install(sql, sharedReadLargeObjectFunction, ddl);
+        log.info("Created shared function {} for capturing table truncate events.", sharedTruncateEventFunction);
     }
+
 
     @Override
     public void dropRequiredDatabaseObjects() {
-        if (installed(SQL_FUNCTION_INSTALLED, sharedTriggersDisabledFunction)) {
+        if (isFunctionInstalled(sharedTriggersDisabledFunction)) {
             uninstall(SQL_DROP_FUNCTION + "() cascade", sharedTriggersDisabledFunction);
         }
-        if (installed(SQL_FUNCTION_INSTALLED, sharedNodeDisabledFunction)) {
+        if (isFunctionInstalled(sharedNodeDisabledFunction)) {
             uninstall(SQL_DROP_FUNCTION + "() cascade", sharedNodeDisabledFunction);
         }
         String largeObjects = this.parameterService.getTablePrefix() + "_" + "largeobject";
-        if (installed(SQL_FUNCTION_INSTALLED, largeObjects)) {
+        if (isFunctionInstalled(largeObjects)) {
             uninstall(SQL_DROP_FUNCTION + "(objectId oid) cascade", largeObjects);
         }
         dropSharedTruncateCaptureFunctions();
     }
 
     public void dropSharedTruncateCaptureFunctions() {
-        if (!parameterService.is(ParameterConstants.TRIGGER_CAPTURE_DDL_CHANGES)) {
-            log.debug("Skipped removing shared function {} because {} parameter is off.", sharedTruncateEventFunction,
-                    ParameterConstants.TRIGGER_CAPTURE_DDL_CHANGES);
-            return;
-        }
-        try {
-            String[] schemas = getAllSchemas();
-            if (schemas == null || schemas.length < 1) {
-                return;
-            }
-            for (String schema : schemas) {
-                if (schema == null || schema.length() < 1) {
-                    continue;
-                }
-                if (installed(SQL_FUNCTION_INSTALLED_FOR_SCHEMA + "'" + schema + "'", sharedTruncateEventFunction)) {
-                    String currentSchemaSharedTruncateEventFunction = "\"" + schema + "\".\"" + sharedTruncateEventFunction + "\"";
-                    uninstall(SQL_DROP_FUNCTION + "() cascade", currentSchemaSharedTruncateEventFunction);
-                }
-            }
-        } catch (Exception ex) {
-            log.error("Failed to check shared function for monitoring truncate events in schemas with tables configured for replication.", ex);
-        }
-    }
+         PostgreSqlTriggerTemplate templatesMap = (PostgreSqlTriggerTemplate) this.triggerTemplate;
+         String sharedTruncateEventFunction = templatesMap.getTruncateSharedFunctionName();
+         if (isFunctionInstalled(sharedTruncateEventFunction)) {
+             uninstall(SQL_DROP_FUNCTION + "() cascade", sharedTruncateEventFunction);
+             log.error("Removed shared function for capturing table truncate events={}", sharedTruncateEventFunction);
+         }
+     }
 
     @Override
     public boolean requiresAutoCommitFalseToSetFetchSize() {
@@ -451,5 +361,9 @@ public class PostgreSqlSymmetricDialect extends AbstractSymmetricDialect impleme
     @Override
     public long getCurrentSequenceValue(SequenceIdentifier identifier) {
         return platform.getSqlTemplate().queryForLong("select last_value from " + getSequenceName(identifier) + "_seq");
+    }
+
+    public boolean isFunctionInstalled(String functionName) {
+        return installed(SQL_FUNCTION_INSTALLED, functionName);
     }
 }
