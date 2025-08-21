309a424a506b740f7ed68c8b8d67b332cee43a6b
Andy W <awebb@jumpmind.com>
2025-08-20 22:24:59 -0400

    0007024: Fix LOB handling parameter for Turkish locale and add unit tests.

--- symmetric-client/src/main/java/org/jumpmind/symmetric/ClientSymmetricEngine.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/ClientSymmetricEngine.java
@@ -384,8 +384,9 @@ public class ClientSymmetricEngine extends AbstractSymmetricEngine {
         if (settings.getOverrideIsolationLevel() >= 0) {
             log.info("Overriding isolation level to " + settings.getOverrideIsolationLevel());
         }
-        settings.setJdbcLobHandling(SqlTemplateSettings.JdbcLobHandling.valueOf(properties.get(ParameterConstants.DBDIALECT_ORACLE_JDBC_LOB_HANDLING,
-                FormatUtils.upper(SqlTemplateSettings.JdbcLobHandling.PLAIN.name()))));
+        settings.setJdbcLobHandling(SqlTemplateSettings.JdbcLobHandling.valueOf(
+                FormatUtils.upper(properties.get(ParameterConstants.DBDIALECT_ORACLE_JDBC_LOB_HANDLING,
+                        SqlTemplateSettings.JdbcLobHandling.PLAIN.name()))));
         settings.setProperties(properties);
         return settings;
     }
