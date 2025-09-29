ecf7cd87b516693d19243150d0dee18854c938f8
Eric Long <elong@jumpmind.com>
2025-09-29 13:40:36 -0400

    disable external entities

--- symmetric-db/src/test/java/org/jumpmind/db/model/DatabaseXmlTest.java
+++ symmetric-db/src/test/java/org/jumpmind/db/model/DatabaseXmlTest.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -86,6 +87,17 @@ public class DatabaseXmlTest {
             }
             assertTrue(symmetricDtdFile.exists());
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setAttribute(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+            // factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            factory.setXIncludeAware(false);
+            factory.setExpandEntityReferences(false);
+            factory.setXIncludeAware(false);
+            factory.setExpandEntityReferences(false);
             // Enable DTD validation:
             factory.setValidating(true);
             DocumentBuilder builder = factory.newDocumentBuilder();
