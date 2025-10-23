29f66ab1cf9602cc9ca832e9bc44c86052e117c5
Philip Marzullo <pmarzullo@jumpmind.com>
2025-10-23 09:55:18 -0400

    0007097: Route data should handle missing Node entries from the cache

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RouterService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/RouterService.java
@@ -920,7 +920,7 @@ public class RouterService extends AbstractService implements IRouterService, IN
                             // Missing some nodes from the cache
                             // Clear cache and try again
                             engine.getNodeService().flushNodeGroupCache();
-                            context.getAvailableNodes().put(triggerRouter, null);
+                            context.getAvailableNodes().remove(triggerRouter);
                             nodeIds = CollectionUtils.intersection(targetNodeIdsList, toNodeIds(findAvailableNodes(triggerRouter, context)));
                         }
                         if (nodeIds.size() == 0 && log.isDebugEnabled()) {
