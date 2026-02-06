1dd3442033d5441388e9bfeefc7ecdbb011188a6
Sai-Gumpalli <sai@jumpmind.com>
2026-02-06 10:34:30 -0500

    Update AbstractRouterServiceTest.java

--- symmetric-core/src/test/java/org/jumpmind/symmetric/service/impl/AbstractRouterServiceTest.java
+++ symmetric-core/src/test/java/org/jumpmind/symmetric/service/impl/AbstractRouterServiceTest.java
@@ -846,7 +846,6 @@ abstract public class AbstractRouterServiceTest extends AbstractServiceTest {
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                 }
@@ -862,7 +861,6 @@ abstract public class AbstractRouterServiceTest extends AbstractServiceTest {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
