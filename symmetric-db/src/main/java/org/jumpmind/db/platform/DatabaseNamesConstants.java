44bd1f75c984da519d06ff7aff26ea0503815215
mdrouard-jumpmind <mdrouard@jumpmind.com>
2026-04-23 12:38:57 -0400

    OS component of Adding FileSync to configure Cavnas

--- symmetric-db/src/main/java/org/jumpmind/db/platform/DatabaseNamesConstants.java
+++ symmetric-db/src/main/java/org/jumpmind/db/platform/DatabaseNamesConstants.java
@@ -80,4 +80,5 @@ final public class DatabaseNamesConstants {
     public final static String EXASOL = "exasol";
     public final static String SPARKSQL = "sparksql";
     public final static String DATABRICKS = "databricks";
+    public final static String FILESYNC = "filesync";
 }
