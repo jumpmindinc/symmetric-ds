cd3cf952d2cc961c17a1b159fd90491646d0b721
philipmarzullo64 <philipmarzullo64@gmail.com>
2026-04-24 13:45:14 -0400

    SYM-7544: sonar fixes

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TransformService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TransformService.java
@@ -225,18 +225,15 @@ public class TransformService extends AbstractService implements ITransformServi
     }
 
     @Override
-    public List<TransformTableNodeGroupLink> findTransformsFor(String sourceNodeGroupId, String targetNodeGroupId, String table, String schema, String catalog) {
+    public List<TransformTableNodeGroupLink> findTransformsFor(String sourceNodeGroupId, String targetNodeGroupId, String table, String schema,
+            String catalog) {
         NodeGroupLink nodeGroupLink = new NodeGroupLink(sourceNodeGroupId, targetNodeGroupId);
         List<TransformTableNodeGroupLink> transformsForNodeGroupLink = findTransformsFor(nodeGroupLink);
         if (!CollectionUtils.isEmpty(transformsForNodeGroupLink)) {
-            List<TransformTableNodeGroupLink> transforms = new ArrayList<TransformTableNodeGroupLink>();
+            List<TransformTableNodeGroupLink> transforms = new ArrayList<>();
             for (TransformTableNodeGroupLink transform : transformsForNodeGroupLink) {
-                if (Strings.CI.equals(catalog, transform.getSourceCatalogName())) {
-                    if (Strings.CI.equals(schema, transform.getSourceSchemaName())) {
-                        if (Strings.CI.equals(table, transform.getSourceTableName())) {
-                            transforms.add(transform);
-                        }
-                    }
+                if (doesTransformApply(transform, catalog, schema, table)) {
+                    transforms.add(transform);
                 }
             }
             if (!transforms.isEmpty()) {
@@ -244,6 +241,12 @@ public class TransformService extends AbstractService implements ITransformServi
             }
         }
         return null;
+    }
+
+    private boolean doesTransformApply(TransformTableNodeGroupLink transform, String catalog, String schema, String table) {
+        return Strings.CI.equals(catalog, transform.getSourceCatalogName()) &&
+                Strings.CI.equals(schema, transform.getSourceSchemaName()) &&
+                Strings.CI.equals(table, transform.getSourceTableName());
     }
 
     @Override
