296ab82b3f0adcd2041196b6a6da0ec3e8ea4aef
Pavel Belov <pbelov@jumpmind.com>
2026-05-04 16:43:32 -0400

    Support overloading RefreshBacklogReportJob in PRO edition

--- symmetric-client/src/main/java/org/jumpmind/symmetric/job/BuiltInJobs.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/job/BuiltInJobs.java
@@ -49,7 +49,7 @@ public class BuiltInJobs {
         builtInJobs.add(new IncomingPurgeJob(engine, taskScheduler));
         builtInJobs.add(new StatisticFlushJob(engine, taskScheduler));
         builtInJobs.add(new PurgeMetricStatsJob(engine, taskScheduler));
-        builtInJobs.add(new RefreshBacklogReportJob(engine, taskScheduler));
+        builtInJobs.add(createJob(RefreshBacklogReportJob.class, engine, taskScheduler));
         builtInJobs.add(new SyncTriggersJob(engine, taskScheduler));
         builtInJobs.add(new HeartbeatJob(engine, taskScheduler));
         builtInJobs.add(new WatchdogJob(engine, taskScheduler));
