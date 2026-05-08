e6b3c2cbfcdcfd7bd2b50a5132ae08c6abb4f9a6
Andrew Webb <awebb@jumpmind.com>
2026-05-08 12:03:45 -0400

    SYM-7589: ignore stale outgoing heartbeat batches. (#776)

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/IOutgoingBatchService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/IOutgoingBatchService.java
@@ -119,7 +119,13 @@ public interface IOutgoingBatchService {
 
     public int countOutgoingBatchesUnsent(String channelId);
 
+    /**
+     * Replaced by {@link #cancelStaleHeartbeatBatches()}.
+     */
+    @Deprecated(forRemoval = true)
     public int countOutgoingBatchesUnsentHeartbeat();
+
+    public void cancelStaleHeartbeatBatches();
 
     public Map<String, Integer> countOutgoingBatchesPendingByChannel(String nodeId);
 
