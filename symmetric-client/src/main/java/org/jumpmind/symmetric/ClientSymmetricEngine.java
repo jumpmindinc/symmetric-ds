2fa5b4802a4766ba233113c7d04ad328feb250d9
Pavel Belov <pbelov@jumpmind.com>
2026-05-20 23:32:18 -0400

    Extra var in createDatabasePlatform

--- symmetric-client/src/main/java/org/jumpmind/symmetric/ClientSymmetricEngine.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/ClientSymmetricEngine.java
@@ -322,7 +322,7 @@ public class ClientSymmetricEngine extends AbstractSymmetricEngine {
         properties.collectFrom("", ParameterConstants.ALL_GOOGLE_BIG_QUERY_PARAMS, parameterService::getString);
         properties.collectFrom("", ParameterConstants.ALL_MONGODB_PARAMS, parameterService::getString);
         properties.collectFrom("", ParameterConstants.ALL_COSMOS_PARAMS, parameterService::getString);
-        return properties;
+        return properties; 
     }
 
     @Override
@@ -333,7 +333,6 @@ public class ClientSymmetricEngine extends AbstractSymmetricEngine {
 
     public static IDatabasePlatform createDatabasePlatform(ApplicationContext springContext, TypedProperties properties,
             DataSource dataSource, boolean waitOnAvailableDatabase) {
-        boolean delimitedIdentifierMode = properties.is(ParameterConstants.DB_DELIMITED_IDENTIFIER_MODE, true);
         return createDatabasePlatform(springContext, properties, dataSource, waitOnAvailableDatabase, properties.is(ParameterConstants.NODE_LOAD_ONLY),
                 properties.is(ParameterConstants.START_LOG_MINER_JOB), null);
     }
