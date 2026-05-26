9d1f7c6b3bc3b7c7cac2379acf673cd86ca148dd
Pavel Belov <pbelov@jumpmind.com>
2026-05-25 22:14:42 -0400

    Clean debug log TypedProperties

--- symmetric-util/src/main/java/org/jumpmind/properties/TypedProperties.java
+++ symmetric-util/src/main/java/org/jumpmind/properties/TypedProperties.java
@@ -229,7 +229,7 @@ public class TypedProperties extends Properties {
     public void logAllKeys(String source) {
         log.debug("Properties source={}, count={}", source, size());
         for (String key : stringPropertyNames()) {
-            log.debug("  {}={}", key, getProperty(key));
+            log.debug("Property {}={}, Source={}", key, getProperty(key), source);
         }
     }
 
