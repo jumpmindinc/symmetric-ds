59b30c1f0551466219c035ce50deb2fdd6670d26
Pavel Belov <pbelov@jumpmind.com>
2026-07-16 17:25:14 -0400

    Fix import

--- symmetric-core/src/test/java/org/jumpmind/symmetric/cache/ClusterMessageConverterTest.java
+++ symmetric-core/src/test/java/org/jumpmind/symmetric/cache/ClusterMessageConverterTest.java
@@ -23,6 +23,7 @@ package org.jumpmind.symmetric.cache;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
