bbe7d7bbf5e80d80f0f97d0342dfd45f3ff2ecf7
evan-miller-jumpmind <70151986+evan-miller-jumpmind@users.noreply.github.com>
2026-07-27 08:10:41 -0400

    SYM-7815: Addressed Sonar warnings in RouterServiceTest

--- symmetric-core/src/test/java/org/jumpmind/symmetric/service/impl/RouterServiceTest.java
+++ symmetric-core/src/test/java/org/jumpmind/symmetric/service/impl/RouterServiceTest.java
@@ -20,10 +20,10 @@
  */
 package org.jumpmind.symmetric.service.impl;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
