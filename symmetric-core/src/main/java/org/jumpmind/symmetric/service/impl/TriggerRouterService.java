0497dfa1e0ec8c6d3a0d427cabe378b1764c57b8
Sai-Gumpalli <sai@jumpmind.com>
2026-08-26 13:54:32 -0400

    Update TriggerRouterService.java

--- symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TriggerRouterService.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/service/impl/TriggerRouterService.java
@@ -2024,8 +2024,14 @@ public class TriggerRouterService extends AbstractService implements ITriggerRou
     }
 
     public boolean syncTriggers(String targetExternalId, boolean force) {
+    	try {
+			Thread.sleep(180000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	if(!activeSyncTriggersNodes.add(targetExternalId)) {
-    		log.info("Sync Triggers already running for node {}", targetExternalId);
+    		log.info("Sync Triggers is already running for node {}", targetExternalId);
     		return true;
     	}
         if (cacheManager.isUsingTargetExternalId(false)) {
