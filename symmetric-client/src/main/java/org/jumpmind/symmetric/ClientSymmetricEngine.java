2a3724b15d4518eb68458c7cd8aa12f3995d338e
Sai-Gumpalli <sai@jumpmind.com>
2026-09-21 13:50:58 -0400

    Update ClientSymmetricEngine.java

--- symmetric-client/src/main/java/org/jumpmind/symmetric/ClientSymmetricEngine.java
+++ symmetric-client/src/main/java/org/jumpmind/symmetric/ClientSymmetricEngine.java
@@ -561,9 +561,10 @@ public class ClientSymmetricEngine extends AbstractSymmetricEngine {
 	private static void logConnectionPoolSize(DataSource dataSource, TypedProperties properties) {
 		int concurrentWorkersMax = properties.getInt(ParameterConstants.CONCURRENT_WORKERS, 20);
 		int effectivePoolSize = IPooledDataSource.of(dataSource).getMaxTotal();
-		log.info("DB connection pool size = {}", effectivePoolSize);
 		if((concurrentWorkersMax * 2) > effectivePoolSize) {
-			log.warn("Configured http.concurrent.workers.max is {}, the effective connection pool size of the database is {}", concurrentWorkersMax, effectivePoolSize);
+			log.warn("Configured {} is {}, the effective connection pool size of the database is {}", ParameterConstants.CONCURRENT_WORKERS, concurrentWorkersMax, effectivePoolSize);
+		}else {
+			log.info("DB connection pool size = {}", effectivePoolSize);
 		}
 	}
 }
