4f936aaec2a4b4e99224cbdf5b1d5f62a30ffca4
Andrew Webb <awebb@jumpmind.com>
2026-09-25 10:44:29 -0400

    Update DBFField.java

--- symmetric-core/src/main/java/org/jumpmind/symmetric/route/parse/DBFField.java
+++ symmetric-core/src/main/java/org/jumpmind/symmetric/route/parse/DBFField.java
@@ -144,8 +144,9 @@ public class DBFField {
             if (obj == null) {
                 obj = Boolean.valueOf(false);
             }
-            if (obj instanceof Boolean flag) {
-                return flag ? "Y" : "N";
+            if (obj instanceof Boolean) {
+                boolean boolean1 = (Boolean) obj;
+                return boolean1 ? "Y" : "N";
             } else {
                 throw new DBFException("Expected a Boolean, got " + obj.getClass() + ".");
             }
